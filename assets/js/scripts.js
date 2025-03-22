ff85ab47bf1f078765a6492846756e8299f0cb82
Klearn20 <59652329+blackknigtt@users.noreply.github.com>
2025-03-22 21:05:08 +0800

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -129,22 +129,14 @@ document.addEventListener("DOMContentLoaded", function () {
     
     
 
-    scrollContainer.addEventListener("mousedown", (e) => {
-        isDown = true;
-        scrollContainer.classList.add("active");
-        startX = e.pageX - scrollContainer.offsetLeft;
-        scrollLeft = scrollContainer.scrollLeft;
-    });
+    
 
     scrollContainer.addEventListener("mouseleave", () => {
         isDown = false;
         scrollContainer.classList.remove("active");
     });
 
-    scrollContainer.addEventListener("mouseup", () => {
-        isDown = false;
-        scrollContainer.classList.remove("active");
-    });
+    
 
     scrollContainer.addEventListener("mousemove", (e) => {
         if (!isDown) return;
